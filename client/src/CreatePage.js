fd3f95641a7748e7c32736f91f8789563543bccc
Thomas Bean <kennette21@gmail.com>
2020-07-20 00:41:51 +0200

    remove comment and trying to push to test my power

--- client/src/CreatePage.js
+++ client/src/CreatePage.js
@@ -49,7 +49,7 @@ class CreatePage extends Component {
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
             mode: 'cors', // no-cors, *cors, same-origin
             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'same-origin', // include, *same-origin, omit
+            credentials: 'same-origin', 
             headers: {
                 'Content-Type': 'application/json'
             },
